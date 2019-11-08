703f206be0ba0c73510ed7c413ecbbde05dc9b93
avitex <theavitex@gmail.com>
2019-11-08 17:41:25 +1100

    rename to aliasable-deref, clean up no-std support

--- src/lib.rs
+++ src/lib.rs
@@ -3,77 +3,71 @@
 #![no_std]
 #![deny(missing_debug_implementations, missing_docs)]
 
-#[cfg(any(feature = "std", test))]
-mod core {
-    extern crate std;
-
-    pub use std::{ops::Deref, ptr::NonNull, rc::Rc, sync::Arc};
-}
-
-#[cfg(all(not(feature = "std"), not(test)))]
-mod core {
-    extern crate core;
-    #[cfg(feature = "alloc")]
-    extern crate alloc;
-
-    pub use core::ops::{ops::Deref, ptr::NonNull};
-
-    #[cfg(feature = "alloc")]
-    pub use alloc::{rc::Rc, sync::Arc};
-}
-
-use self::core::*;
+use core::ops::Deref;
 
 /// Marker trait for a pointer type that is allowed to have its
 /// pointee aliased (except when dropped).
-pub unsafe trait AliasableOwner: Deref {}
+pub unsafe trait AliasableDeref: Deref {}
 
 #[cfg(any(feature = "alloc", feature = "std"))]
-unsafe impl<T: ?Sized> AliasableOwner for Rc<T> {}
+unsafe impl<T: ?Sized> AliasableDeref for self::alloc::Rc<T> {}
 
 #[cfg(any(feature = "alloc", feature = "std"))]
-unsafe impl<T: ?Sized> AliasableOwner for Arc<T> {}
+unsafe impl<T: ?Sized> AliasableDeref for self::alloc::Arc<T> {}
 
 /// Helper trait for converting non-aliasable types
 /// into their aliasable counterparts.
-pub trait IntoAliasableOwner {
+pub trait IntoAliasable {
     /// The aliasable type to convert to.
-    type Target: AliasableOwner;
+    type Target: AliasableDeref;
 
     /// Convert into an aliasable pointer type.
-    fn into_aliasable_owner(self) -> Self::Target;
+    fn into_aliasable(self) -> Self::Target;
 }
 
-impl<T> IntoAliasableOwner for T
+impl<T> IntoAliasable for T
 where
-    T: AliasableOwner
+    T: AliasableDeref
 {
     type Target = T;
 
-    fn into_aliasable_owner(self) -> Self::Target {
+    fn into_aliasable(self) -> Self::Target {
         self
     }
+}
+
+#[cfg(any(feature = "std", test))]
+mod alloc {
+    extern crate std;
+    pub use std::{rc::Rc, sync::Arc};
+}
+
+#[cfg(all(feature = "alloc", not(feature = "std"), not(test)))]
+mod alloc {
+    extern crate alloc;
+    pub use alloc::{rc::Rc, sync::Arc};
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::alloc::*;
 
     #[test]
     fn test_rc() {
-        let ptr = &Rc::new(()) as &dyn AliasableOwner<Target = ()>;
+        let ptr = &Rc::new(()) as &dyn AliasableDeref<Target = ()>;
         assert_eq!(ptr.deref(), &());
     }
 
     #[test]
     fn test_arc() {
-        let ptr = &Arc::new(()) as &dyn AliasableOwner<Target = ()>;
+        let ptr = &Arc::new(()) as &dyn AliasableDeref<Target = ()>;
         assert_eq!(ptr.deref(), &());
     }
 
     #[test]
     fn test_into_aliasable_owner() {
-        let aliasable_ptr = Arc::new(()).into_aliasable_owner();
+        let aliasable_ptr = Arc::new(()).into_aliasable();
         assert_eq!(aliasable_ptr.deref(), &());
     } 
 }
